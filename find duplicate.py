d5e22965fd4d4815ccf6d31ccbf89c1363ba76a1
PrathamSharma169 <prathamsharma0916@gmail.com>
2023-10-05 20:26:02 +0530

    to find duplicate number present in a list

--- find duplicate.py
+++ find duplicate.py
@@ -4,6 +4,6 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i]==nums[i+1]:
                 return nums[i]
-s=[1,2,3,2]
+p=[1,2,3,2]
 cl=Solution()
-print(cl.findDuplicate(s))
+print(cl.findDuplicate(p))
